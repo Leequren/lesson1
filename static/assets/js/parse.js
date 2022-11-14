223477dbda3ddd7ac821fe17f16eba5b5e3f70ae
Rubik <babichnr@bk.ru>
2022-11-14 12:53:00 +0300

    com

--- static/assets/js/parse.js
+++
@@ -1,8 +0,0 @@
-let personalArea = document.querySelector('.personalArea');
-let input = document.querySelector('input');
-let btn = document.querySelector('.search');
-console.log(btn);
-input.addEventListener('input', (event)=>{
-    event.preventDefault();
-    btn.setAttribute('href', `/parse/${input.value}`);
-});
